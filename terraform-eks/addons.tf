2e62323765a4739fe828923778e4fe0e1216e222
Parth2496Singh <parthsinghkushwaha24@gmail.com>
2026-07-17 20:36:31 +0530

    New updated chart for microserivces

--- terraform-eks/addons.tf
+++ terraform-eks/addons.tf
@@ -84,7 +84,16 @@ module "eks_addons_apps" {
               requests = { cpu = "100m", memory = "256Mi" }
             }
           }
-          ingress = { enabled = false }
+          ingress = {
+            enabled          = true
+            ingressClassName = "alb"
+            annotations = {
+              "alb.ingress.kubernetes.io/scheme"      = "internet-facing"
+              "alb.ingress.kubernetes.io/target-type" = "ip"
+            }
+            hosts = [""]
+            paths = ["/*"]
+          }
         }
         grafana = {
           adminPassword = "prom-operator"
